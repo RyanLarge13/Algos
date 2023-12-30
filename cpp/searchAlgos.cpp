68568a0da30247eef135f7a09c6577763aad9ec6
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-12-29 19:29:06 -0500

    Ternary, Quadratic, Exponential Search
    Features:
    
    Adding in new search algorithms for searching pre sorted arrays.

--- cpp/searchAlgos.cpp
+++ cpp/searchAlgos.cpp
@@ -41,7 +41,7 @@ int binarySearch(int arr[], int val) {
 int jumpSearch(int arr[], int val) {
 	int low = 0;
 	int high = mySize;
-	int jump = 100;
+	int jump = 50000;
 	while (low < high) {
 		int next = low + jump;
 		if (next >= mySize) {
@@ -64,9 +64,87 @@ int jumpSearch(int arr[], int val) {
 	return -1;
 }
 
-//int exponentialSearch(int arr[], int val) {}
 
-//int ternarySearch(int arr[], int val) {}
+int ternarySearch(int arr[], int val) {
+	int low = 0;
+	int high = mySize;
+	while (low < high) {
+		int firstMax = low + (high - low) / 3;
+		int secondMax = low + 2 * (high - low) / 3;
+		if (val == arr[low] || val == arr[high]) {
+			return val;
+		}
+		if (arr[firstMax] == val) {
+			return firstMax;
+		}
+		if (arr[secondMax] == val) {
+			return secondMax;
+		}
+		if (val < firstMax) {
+			high = firstMax;
+		}
+		if (val > firstMax && val < secondMax) {
+			high = secondMax;
+			low = firstMax + 1;
+		}
+		if (val > secondMax && val < high) {
+			low = secondMax + 1;
+		}
+		complexOp();
+	}
+	return -1;
+}
+
+int quadraticSearch(int arr[], int val) {
+	int low = 0;
+	int high = mySize;
+	while (low < high) {
+		int firstMax = low + (high - low) / 4;
+		int secondMax = low + 2 * (high - low) / 3;
+		int thirdMax = low + 3 * (high - low) / 3;
+		if (val == arr[low] || val == arr[high]) {
+			return val;
+		}
+		if (arr[firstMax] == val) {
+			return firstMax;
+		}
+		if (arr[secondMax] == val) {
+			return secondMax;
+		}
+		if (arr[thirdMax] == val) {
+			return secondMax;
+		}
+		if (val < firstMax) {
+			high = firstMax;
+		}
+		if (val > firstMax && val < secondMax) {
+			high = secondMax;
+			low = firstMax + 1;
+		}
+		if (val > secondMax && val < thirdMax) {
+			low = secondMax + 1;
+		}
+		if (val > secondMax && val < high) {
+			low = thirdMax + 1;
+		}
+		complexOp();
+	}
+	return -1;
+}
+
+int exponentialSearch(int arr[], int val) {
+	if (arr[0] == val) {
+		return 0;
+	}
+	int range = 1;
+	while (range < mySize && arr[range] <= val) {
+		range *= 2;
+	}
+	int start = range / 2;
+	int end = min(range, mySize - 1);
+
+	return 0; // binarySearch(); can't be called with the new array length, implement your own custom exponentialSearch binarySearch function
+}
 
 int linearSearch(int arr[], int val) {
 	for (int i = 0; i < mySize; i++) {
@@ -83,26 +161,50 @@ int main() {
 	{
 		// Linear search calculation block
 		auto start = chrono::high_resolution_clock::now();
-		linearSearch(myArr, 500);
+		linearSearch(myArr, 100);
 		auto end = chrono::high_resolution_clock::now();
 		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
-		cout << "Linear search runtime: " << duration.count() << endl;
+		cout << "Linear search runtime:      " << duration.count() << endl;
+	}
+	{
+		// Exponential search calculation block
+		auto start = chrono::high_resolution_clock::now();
+		exponentialSearch(myArr, 20);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
+		cout << "Exponential search runtime: " << duration.count() << endl;
 	}
 	{
 		// Binary search calculation block
 		auto start = chrono::high_resolution_clock::now();
-		binarySearch(myArr, 500);
+		binarySearch(myArr, 250001);
 		auto end = chrono::high_resolution_clock::now();
 		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
-		cout << "Binary search runtime: " << duration.count() << endl;
+		cout << "Binary search runtime:      " << duration.count() << endl;
 	}
 	{
 		// Jump search calculation block
 		auto start = chrono::high_resolution_clock::now();
-		jumpSearch(myArr, 500);
+		jumpSearch(myArr, 250001);
 		auto end = chrono::high_resolution_clock::now();
 		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
-		cout << "Jump search runtime: " << duration.count() << endl;
+		cout << "Jump search runtime:        " << duration.count() << endl;
+	}
+	{
+		// Ternary search calculation block
+		auto start = chrono::high_resolution_clock::now();
+		ternarySearch(myArr, 250001);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
+		cout << "Ternary search runtime:     " << duration.count() << endl;
+	}
+	{
+		// Quadratic search calculation block
+		auto start = chrono::high_resolution_clock::now();
+		quadraticSearch(myArr, 250001);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast < chrono::microseconds > (end - start);
+		cout << "Quadratic search runtime:   " << duration.count() << endl;
 	}
 	return 0;
 }
