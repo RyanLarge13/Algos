3a6b12bcc9b23cbd19499c980b81d2bef3507e97
RyanLarge13 <ryanlarge13@gmail.com>
2024-01-21 10:24:02 -0500

    More Linked List Functions
    Functionality:
    Creating more methods to use on a linked list, including returning values from removing items in a list
    and removing items at specified placements within the list

--- cpp/linkedList.cpp
+++ cpp/linkedList.cpp
@@ -51,10 +51,75 @@ public:
         delete temp->next;
         temp->next = nullptr;
     }
-    void shift() {
-
+    int getPop() {
+        int returnValue;
+        if (!head) {
+            return -1;
+        }
+        if (!head->next) {
+            returnValue = head->data;
+            delete head;
+            head = nullptr;
+            return returnValue;
+        }
+        Node* temp = head;
+        while (temp->next && temp->next->next) {
+            temp = temp->next;
+        }
+        returnValue = temp->next->data;
+        delete temp->next;
+        temp->next = nullptr;
+        return returnValue;
     }
-    void removeAt(int placement) {
+    void shift() {
+        if (!head) {
+            return;
+        }
+        if (!head->next) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node* temp = head;
+        head = temp->next;
+        delete temp;
+    }
+    int getShift() {
+        int returnValue;
+        if (!head) {
+            return -1;
+        }
+        if (!head->next) {
+            returnValue = head->data;
+            delete head;
+            head = nullptr;
+            return returnValue;
+        }
+        returnValue = head->data;
+        Node* temp = head;
+        head = temp->next;
+        delete temp;
+        return returnValue;
+    }
+    int removeAt(int placement) {
+        int iteration;
+        if (!head) {
+            return -1;
+        }
+        if (!head->next && placement > 0) {
+            return -1;
+        }
+        Node* temp = head;
+        if (placement == 0) {
+            delete temp;
+            head = nullptr;
+            return head->data;
+        }
+        while (temp->next) {
+
+        }
+    }
+    int removeValue(int value) {
 
     }
     void display() {
@@ -72,10 +137,18 @@ int main() {
     myList.append(0);
     myList.prepend(10);
     myList.prepend(20);
+    myList.shift();
     myList.append(1);
     myList.append(2);
     myList.append(3);
+    myList.append(4);
+    myList.append(5);
     myList.pop();
+    int myPoppedValue = myList.getPop();
+    int myShiftedValue = myList.getShift();
+    myList.shift();
     myList.display();
+    cout << "Popped value: " << myPoppedValue << endl;
+    cout << "Shifted value: " << myShiftedValue << endl;
     return 0;
 }
