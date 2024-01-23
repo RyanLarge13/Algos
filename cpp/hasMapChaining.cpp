871058e2f96e6649cd398eba5728512e455f92a4
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-22 19:11:58 -0500

    Hash Map

---
+++ cpp/hasMapChaining.cpp
@@ -0,0 +1,18 @@
+struct Node {
+	string value;
+	Node* next;
+	Node(string data): value(data);
+	next(nullptr) {}
+}
+
+int hash(string key) {
+	
+}
+
+class HashMap {
+	void appendValues()
+}
+
+int main() {
+	return 0;
+}
