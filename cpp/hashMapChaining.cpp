3cd7e3cb875bcd461708d57faeeb545494dce620
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-23 18:14:53 -0500

    Hash Map
    Functionality:
    Hash map search and hash map insert
    methods now work! Not much is more fun that
    taking a concept and turning into code

--- cpp/hashMapChaining.cpp
+++ cpp/hashMapChaining.cpp
@@ -12,22 +12,92 @@ struct Link {
 	value(val) {}
 };
 
-class hashMap {
+class HashMap {
 	private:
-	static const int size = 10;
-	Link* table[size];
+	static const int size = 1000;
+	Link* table[size] {
+		nullptr
+	};
 	int hashFunc(string key, int length) {
-		int len = key.length();
-		return len % length;
+		int hash = key.length() % length;
+		return hash;
 	}
 	public:
-	void insert() {} 
-	void search() {} 
-	void remove() {} 
-	void print() {} 
+	void insert(string key, int value) {
+		int hashIndex = hashFunc(key, size);
+		Link* newLink = new Link(nullptr, key, value);
+		if (table[hashIndex] != nullptr) {
+			Link* temp = table[hashIndex];
+			if (!temp->nextLink) {
+				temp->nextLink = newLink;
+				return;
+			}
+			while(temp->nextLink) {
+				if (temp->nextLink->nextLink == nullptr) {
+					temp->nextLink->nextLink = newLink;
+					return;
+				}
+				temp = temp->nextLink;
+			}
+			return;
+		}
+		table[hashIndex] = newLink;
+		return;
+	}
+	void search(string key) {
+		int index = hashFunc(key, 10);
+		Link* tableValue = table[index];
+		if (tableValue != nullptr) {
+			if (tableValue->key == key) {
+				cout << tableValue->value;
+				return;
+			}
+			Link* temp = tableValue;
+			while(temp->nextLink) {
+				Link* next = temp->nextLink;
+				if (next->key == key) {
+					cout << next->value;
+					return;
+				}
+				temp = temp->nextLink;
+			}
+			return;
+		}
+		cout << "No value at this index";
+		return;
+	}
+	void remove(string key) {
+		
+	}
+	void print() {
+		string tab = " ";
+		cout << "{" << endl;
+		for (Link* link: table) {
+			if (link != nullptr) {
+				cout << tab << link->key << ": " << link->value << endl;
+				if (link->nextLink != nullptr) {
+					Link* temp = link;
+					while(temp->nextLink) {
+						Link* currentLink = temp->nextLink;
+						cout << tab << currentLink->key << ": " << currentLink->value << endl;
+						temp = currentLink;
+					}
+				}
+			}
+		}
+		cout << "}" << endl;
+	}
 };
 
 
 int main() {
+	HashMap myNewMap;
+	myNewMap.insert("Ryan", 28);
+	myNewMap.insert("Bill", 34);
+	myNewMap.insert("Joey", 60);
+	myNewMap.insert("Billy", 37);
+	myNewMap.insert("Joseph", 18);
+	myNewMap.print();
+	myNewMap.search("Joey");
 	return 0;
 }
