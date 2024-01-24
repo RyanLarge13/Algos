0a64fbda74fa75e44b5c6a1558ff56e253f40fab
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-24 18:23:13 -0500

    Doubley Linked List
    Finally fixed the insersion bug.
    This stuff is so much fun!! I love it
    This list inserts new values at the "beginning" of the list to make insertion easy

--- cpp/dblLinkedList.cpp
+++ cpp/dblLinkedList.cpp
@@ -23,10 +23,29 @@ class DoubleyLinkedList {
 			cout << head->value << endl;
 			return;
 		}
-		DblLink* link = head;
-		while (link->nextLink != head) {
-			cout << link->value;
+		DblLink* link = head->nextLink;
+		while (link != head) {
+			DblLink* prevPtr = link->prevLink;
+			DblLink* nextPtr = link->nextLink;
+			cout << "prev: " << prevPtr->value << endl << "current: " << link->value << endl << "next: " << nextPtr->value << endl << endl;
 			link = link->nextLink;
+		}
+		return;
+	}
+	void printReverse() {
+		if (!head) {
+			cout << "Your list is empty" << endl;
+			return;
+		}
+		if (!head->prevLink) {
+			cout << head->value << endl;
+			cout << "No prev link" << endl;
+			return;
+		}
+		DblLink* link = head->prevLink;
+		while (link != head) {
+			cout << link->value << endl;
+			link = link->prevLink;
 		}
 		return;
 	}
@@ -64,24 +83,30 @@ class DoubleyLinkedList {
 		}
 		if (!head->nextLink) {
 			DblLink* newLink = new DblLink(head, head, value);
+			head->nextLink = newLink;
+			head->prevLink = newLink;
 			return;
 		}
+		DblLink* temp = head->nextLink;
 		DblLink* newLink = new DblLink(head, head->nextLink, value);
 		head->nextLink = newLink;
+		temp->prevLink = newLink;
 		return;
 	}
 	void remove() {}
-	// void shift() {}
 };
 
 int main() {
 	DoubleyLinkedList dblList;
-	dblList.print();
-	dblList.findValue(0);
-	dblList.findValue(1);
 	dblList.insert(10);
 	dblList.insert(8);
-	dblList.findValue(10);
+	dblList.insert(9);
+	dblList.insert(5);
+	dblList.insert(1);
+	dblList.insert(3);
+	dblList.insert(7);
 	dblList.print();
+	cout << endl << endl;
+	dblList.printReverse();
 	return 0;
 }
