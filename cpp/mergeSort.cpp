ea2f4efd8b75ad4e1261ab27a61a927d469c8e4c
Ryan Large <RLarge@bostonvalley.com>
2024-01-29 11:56:28 -0500

    Merge Sort

--- cpp/mergeSort.cpp
+++ cpp/mergeSort.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 void merge(vector < int > arr, size_t left, size_t mid, size_t right) {
 	size_t n1 = mid - left + 1;
-	size_t n2;
+	size_t n2 = right - mid;
+	vector <int> subLeft(arr.begin() + left, arr.begin() + left + n1);
+	vector <int> subRight(arr.begin() + mid + 1, arr.begin() + mid + 1+ n2);
+	size_t i = 0, j = 0; k = left;
+	while (i < n1 && j < n2) {
+		if (leftSub[i] <= rightSub[j]) {
+			arr[k] = leftSub[i];
+			++i;
+		} else {
+			arr[k] = rightSub[j];
+			j++;
+		}
+		++k;
+	}
+	while (i < n1) {
+		arr[k] =leftSub[i];
+		++i;
+		++k;
+	}
+	while (j < n2) {
+		arr[k] = rightSub[j];
+		++j;
+		++k;
+	}
 };
 
 void mergeSort(vector < int >& arr, size_t left, size_t right) {
