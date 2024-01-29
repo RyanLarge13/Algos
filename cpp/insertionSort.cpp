d437f5e573cb8efaeb749a927d326a60c9396971
Ryan Large <RLarge@bostonvalley.com>
2024-01-29 12:07:41 -0500

    Insertion Sort

--- cpp/insertionSort.cpp
+++ cpp/insertionSort.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 void insertionSort(int arr[]) {
-	
 	for (int i = 0; i < arr.length(); i++) {
-		
+		int key = arr[i];
+		int j = i - 1;
+		while (j >= 0 && key < arr[j]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
 	}
 }
 
