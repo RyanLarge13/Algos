03386dee9b28242d65bd06fdf8b1393989e79878
Ryan Large <RLarge@bostonvalley.com>
2024-01-30 12:00:23 -0500

    Ropes

--- cpp/rope.cpp
+++ cpp/rope.cpp
@@ -12,27 +12,48 @@ struct RopeNode {
 
 class Rope {
     private:
-    
-    void print(RodeNode* rope) {
-        RopeNode* left = rope->left;
-        RopeNode* right = rope->right;
-        cout << rope->value;
-        while(left && right) {
-            print(right);
-            print(left);
-        }
-    }
 
-    RopeNode mainRoot = new RopeNode(nullptr, nullptr, nullptr, "New document");
+        RopeNode mainRoot = new RopeNode(nullptr, nullptr, nullptr, "New document");
+
+        void print(RodeNode* rope) {
+            RopeNode* left = rope->left;
+            RopeNode* right = rope->right;
+            cout << rope->value;
+            if (left) {
+                print(left);
+            }
+            if (right) {
+                print(right);
+            }
+        }
+
+        RopeNode* findLeaf(RopeNode* node) {
+            RopeNode* left = node->left;
+            RopeNode* right = node->right;
+            string value;
+            if (value.length() > 50) {
+                
+            }
+            return node;
+        }
+
     public:
     
-    void printDocument() {
-        RopeNode* temp = mainRoot;
-          
-    }
+        void printDocument() {
+            printDocument(mainRoot);
+        }
+
+        void printSection(RopeNode* leaf) {
+            print(leaf);
+        }
+
+        void insert(string value, int position) {
+            RopeNode* leafToConcatenate = findLeaf(mainRoot);
+        }
 };
 
 int main() {
     Rope myRope;
+    myRope.printDocument();
     return 0;
 }
