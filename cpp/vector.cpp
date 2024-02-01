94408460a7ea53cade3cfcd20815f748d24049ad
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-02-01 18:10:49 -0500

    What am I doing? 🤔

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <initializer_list>
 using namespace std;
 
 struct Vect {
@@ -12,10 +13,10 @@ class Vector {
   private:
   size_t defaultSize = 0;
   Vect* vec = new Vect(defaultSize);
-  size_t length = sizeof(vec->myArr) / sizeof(vec[0]);
+  size_t length = 0;
   public:
-  template < typename... Nums >
-  void print(Nums... nums) {
+
+  void print() {
     if (length == 0) {
       cout << "Your vect is empty" << endl;
     }
@@ -25,7 +26,32 @@ class Vector {
     }
     cout << ']' << endl;
   }
-  void push() {}
+
+  void push(initializer_list < int > nums) {
+    size_t numsSize = nums.size();
+    if (numsSize < 1) {
+      cout << "Pass in at least 1 value" << endl;
+      return;
+    }
+    if (numsSize >= length) {
+      length = (length == 0 ? 1: length *= 2);
+      delete vec;
+      vec = new Vect(length);
+      push(nums);
+      return;
+    }
+    int newArr[length + numsSize];
+    int tracker = 0;
+    for (int i = 0; i < length; i++) {
+      newArr[i] = vec->myArr[i];
+      tracker++;
+    }
+    for (int num: nums) {
+      newArr[tracker] = num;
+      tracker++;
+    }
+    return;
+  }
   void pop() {}
   void shift() {}
   void find() {}
@@ -33,5 +59,10 @@ class Vector {
 
 int main() {
   Vector myVect;
+  myVect.print();
+  myVect.push({
+    1, 2, 3, 4
+  });
+  myVect.print();
   return 0;
 }
