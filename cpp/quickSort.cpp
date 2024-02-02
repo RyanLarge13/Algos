e56f20fc4b4deff60b34a93a73338497a990f641
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-02-02 13:38:18 -0500

    Quick Sort For Real
    Here is my quick sort implementation.
    Simole algorithm and fairly general.

--- cpp/quickSort.cpp
+++ cpp/quickSort.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class QuickSort {
+  private:
 
-    return 0;
+  vector < int > initialArr;
+
+  int partition(vector < int >& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (arr[j] < pivot) {
+        i++;
+        swap(arr[i], arr[j]);
+      }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+  }
+
+  public:
+
+  void print() {
+    cout << endl;
+    for (int num: initialArr) {
+      cout << num << ", ";
+    }
+    cout << endl;
+  }
+
+  void sort(vector < int >& arr, int low, int high) {
+    if (low < high) {
+    int pivotIndex = partition(arr, low, high);
+    sort(arr, low, pivotIndex - 1);
+    sort(arr, pivotIndex + 1, high);
+    } 
+    initialArr = arr;
+  }
+
+};
+
+int main() {
+  QuickSort qckSrt;
+  vector < int > myArr = {
+    1,
+    6,
+    8,
+    4,
+    0,
+    7,
+    3,
+    8,
+    6,
+    7
+  };
+  qckSrt.sort(myArr, 0, 10);
+  qckSrt.print();
+  return 0;
 }
