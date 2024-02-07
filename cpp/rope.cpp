52171f1d4f83632146faf9bef52c6688f379fa3a
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-02-07 10:25:24 -0500

    Rope Insertion
    Algorithm Methods:
    I am pretty confused about what is going on. But one step at a time.

--- cpp/rope.cpp
+++ cpp/rope.cpp
@@ -1,86 +1,93 @@
 #include <iostream>
 using namespace std;
 struct RopeNode {
-	RopeNode* left;
-	RopeNode* right;
-	RopeNode* parent;
-	string value;
+  RopeNode* left;
+  RopeNode* right;
+  string value;
+  int weight;
 
-	RopeNode(RopeNode* parent, RopeNode* left, RopeNode* right, string val) : parent(parent),
-		left(left),
-		right(right),
-		value(val) {}
+  RopeNode(RopeNode* left, RopeNode* right, string val, int
+    weight):
+  left(left),
+  right(right),
+  value(val),
+  weight(weight) {}
 };
 
 
 class Rope {
-private:
+  private:
 
-	RopeNode* mainRoot = new RopeNode(nullptr, nullptr, nullptr, "New document");
+  RopeNode* mainRoot = new RopeNode(nullptr, nullptr, "", 0);
 
-	RopeNode* leafToAppendTo = findLeaf(mainRoot);
+  void print(RopeNode* rope) {
+    RopeNode* left = rope->left;
+    RopeNode* right = rope->right;
+    cout << rope->value;
+    if (left) {
+      print(left);
+    }
+    if (right) {
+      print(right);
+    }
+  }
 
-	void print(RopeNode* rope) {
-		RopeNode* left = rope->left;
-		RopeNode* right = rope->right;
-		cout << rope->value;
-		if (left) {
-			print(left);
-		}
-		if (right) {
-			print(right);
-		}
-	}
+  void insertString(RopeNode* node, string value, int index) {
+    if (index <= 0) {
+      RopeNode* newNode = new RopeNode(nullptr, nullptr, value, value.length());
+      newNode->weight = value.length();
+      newNode->right = node;
+      mainRoot = newNode;
+      return;
+    }
+    if (index >= node->weight) {
+      if (!node->right) {
+        node->right = new RopeNode(nullptr, nullptr, value, value.length());
+        node->weight += value.length();
+      } else {
+        insertString(node->right, value, index - node->weight);
+        node->weight += value.length();
+      }
+    }
+    else {
+      string currentVal = node->value;
+      RopeNode* newLeft = new RopeNode(nullptr, nullptr, currentVal.substr(0, index), index);
+      RopeNode* newMiddle = new RopeNode(nullptr, nullptr, value, value.length());
+      RopeNode* newRight = new RopeNode(nullptr, nullptr, currentVal.substr(index), currentVal.length() - index);
+      newLeft->weight = index;
+      newMiddle->weight = value.length();
+      newRight->weight = currentVal.length() - index;
 
-	RopeNode* findLeaf(RopeNode* node) {
-		RopeNode* left = node->left;
-		RopeNode* right = node->right;
-		RopeNode* parent = node->parent;
-		string value = node->value;
-		if (value.length() > 50) {
-			if (!left && !right) {
-				node->left = new RopeNode(node, nullptr, nullptr, value);
-				leafToAppendTo = node->left;
-			}
-			if (!left) {
-				return findLeaf(right);
-			}
-			return findLeaf(left);
-		}
-		return node;
-	}
+      node->left = newLeft;
+      newMiddle->right = newRight;
+      newLeft->right = newMiddle;
+      node->value = "";
+      node->weight = index;
+    }
+  }
 
-public:
+  public:
 
-	void printDocument() {
-		print(mainRoot);
-	}
+  void printDocument() {
+    print(mainRoot);
+  }
 
-	void printSection(RopeNode* leaf) {
-		if (!leaf) {
-			cout << mainRoot->value;
-			return;
-		}
-		cout << leaf->value;
-	}
+  void insert(string value, int index) {
+    insertString(mainRoot, value, index);
+  }
 
-	void insert(string value) {
-		leafToAppendTo = findLeaf(mainRoot);
-		leafToAppendTo->value += value;
-	}
+  void deleteAt(int index) {}
 };
 
 int main() {
-	Rope myRope;
-	cout << "Before insert" << endl << "********" << endl;
-	myRope.printDocument();
-	myRope.insert(" This is a test string");
-	myRope.insert(" I need to better understand how Ropes work to know how to program this data structure.");
-	myRope.insert(" To be perfectly honest, I think I am a bit stuck here. It's because I need to read up on this and do some good lessons on rope data structures.");
-	cout << endl << endl << "After insert" << endl << "********" << endl;
-	myRope.printDocument();
-	cout << endl;
-	cout << "Printing only mainroot" << endl;
-	myRope.printSection(nullptr);
-	return 0;
+  Rope myRope;
+  myRope.printDocument();
+  myRope.insert(" This doc is short.", 0);
+  myRope.insert(" end-ish", 5);
+  myRope.printDocument();
+  myRope.insert("Begin!", 0);
+  myRope.insert("What the vi!!!", 4);
+  myRope.printDocument();
+  cout << endl;
+  return 0;
 }
