5e02c0963cb679cc8e8637fb47a0ec7d2ef4526e
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-02-08 22:54:38 -0500

    Gqp Buffer
    Functionality:
    Attempting a text editor with my custom gap buffer that does not handle
    left and right very well.

--- cpp/gapBuffer.cpp
+++ cpp/gapBuffer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "ncurses.h"
 using namespace std;
 
 class GapBuffer {
@@ -33,23 +34,26 @@ class GapBuffer {
   public:
   GapBuffer(int initialSize = 10): buffer(initialSize, ' '),
   gapStart(0),
-  gapEnd(initialSize) {}
+  gapEnd(initialSize),
+  indexState(0) {}
 
   void insert(char c, int index) {
     if (index < 0 || index > buffer.size()) {
       return;
     }
-    if (gapStart != gapEnd)
-    moveGap(index);
+    if (gapStart != gapEnd) {
+      moveGap(index);
+    }
     buffer[gapStart] = c;
     gapStart++;
     indexState++;
-    if (gapStart == gapEnd)
-    expandBuffer();
+    if (gapStart == gapEnd) {
+      expandBuffer();
+    }
   }
 
   void erase(int index) {
-    if (index < 0 || index > buffer.size()) {
+    if (index < 0 || index >= buffer.size()) {
       return;
     }
     if (gapStart != gapEnd) {
@@ -69,22 +73,86 @@ class GapBuffer {
     return indexState;
   }
 
+  void movePosBack(int amount) {
+    indexState -= amount;
+  }
+
+  void movePosForward(int amount) {
+    if (indexState == buffer.size()) {
+      return;
+    }
+    indexState += amount;
+  }
+
+  int getSize() {
+    return buffer.size();
+  }
+
 };
 
 int main() {
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+
   GapBuffer buff;
-  string line;
-  while(getline(cin, line)) {
-    if (line.empty()) {
-      cout << "End of input. Press Enter again to exit." << endl;
-      getline(cin, line);
-      if (line.empty()) {
-        break;
+  bool editing = true;
+  while(editing) {
+    char c = getch();
+    switch (c) {
+      case KEY_LEFT:
+      buff.movePosBack(1);
+      break;
+      case KEY_RIGHT:
+      buff.movePosForward(1);
+      break;
+      case KEY_DC:
+      buff.erase(buff.getCurrentPos());
+      break;
+      case KEY_BACKSPACE:
+      buff.movePosBack(1);
+      buff.erase(buff.getCurrentPos());
+      break;
+      case 127:
+      buff.erase(buff.getCurrentPos());
+      break;
+      case 13:
+      buff.insert('\n', buff.getCurrentPos());
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      if (buff.getCurrentPos() > 0) {
+        buff.movePosBack(1);
       }
-    }
-    for (char c: line) {
+      break;
+      case 5:
+      if (buff.getCurrentPos() <= buff.getSize()) {
+        buff.movePosForward(1);
+      }
+      break;
+      case '\n': // Handle newline
+      // Handle newline as needed
+      break;
+      // tab key
+      case 9:
+      buff.movePosForward(2);
+      break;
+      case 17:
+      editing = false;
+      break;
+      default:
       buff.insert(c, buff.getCurrentPos());
+      break;
     }
+    clear();
+    printw("%s", buff.print().c_str());
+    move(0, buff.getCurrentPos());
+    refresh();
   }
+  endwin();
   return 0;
 }
